60337d1ddae0d04831a1b67dfdbd660193182194
salmakouhou <59792796+salmakouhou@users.noreply.github.com>
2020-04-12 15:06:14 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,16 +9,7 @@ $(document).ready(() => {
   getLatestInfo();
   getAllCountryGraphs();
 });
-var firebaseConfig = {
-    apiKey: "AIzaSyArsZT6la12dxI9VBIpVcPjYXiG5uBqK4A",
-    authDomain: "covid19-salmakouhou.firebaseapp.com",
-    databaseURL: "https://covid19-salmakouhou.firebaseio.com",
-    projectId: "covid19-salmakouhou",
-    storageBucket: "covid19-salmakouhou.appspot.com",
-    messagingSenderId: "339630467989",
-    appId: "1:339630467989:web:2e342709f790a0dc8ea49c",
-    measurementId: "G-FHLKN1FCL6"
-  };
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
