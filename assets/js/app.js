a893aa0a9cdf80f3dd1fe3b25131770cb3d86c69
salmakouhou <59792796+salmakouhou@users.noreply.github.com>
2020-05-31 23:35:58 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ const lastUpdated = document.getElementById("last-updated");
 let starredCountries = [];
 let sortedCountryArray = [];
 import firebase from 'firebase/app';
-import { auth } from 'firebase/app';
+import 'firebase/auth';
 import 'firebase/firestore';
 $(document).ready(() => {
   getLatestInfo();
